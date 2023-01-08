dfc6ec79de5c879f84c44e114ebafe1c17f0ca2b
Sajeeth Ahmad <sajiiahd@gmail.com>
2023-01-08 15:11:48 +0530

    feat: define a function

--- codewars/bope-squad.py
+++ codewars/bope-squad.py
@@ -7,3 +7,12 @@ weapons = {
     "M16A2": 30,
     "PSG1": 5
 }
+
+
+def magazine_no(info: Tuple[str, int]) -> int:
+    (weapon, streets) = info
+    bullt_fire = streets * 3
+    mag_cap = weapons[weapon]
+    need_mag = bullt_fire / mag_cap
+
+    return ceil(need_mag)
